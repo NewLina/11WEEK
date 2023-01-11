771814eac328d88fb6771eae66011ce170998160
Имя <prybak497@gmail.com>
2023-01-11 20:13:20 +0300

    .

---
+++ assignment1/assets/index.js
@@ -0,0 +1,6 @@
+let result=document.querySelector('.result');
+let sumOne=document.querySelector('.sum1');
+let sumTwo=document.querySelector('.sum2');
+let sumThree=document.querySelector('.sum3');
+let sumFour=document.querySelector('.sum4');
+
