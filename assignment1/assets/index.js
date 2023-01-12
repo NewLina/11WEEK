929ac1510e022ada1687081586e8d3d7a002893b
Имя <prybak497@gmail.com>
2023-01-12 12:41:26 +0300

    done

--- assignment1/assets/index.js
+++ assignment1/assets/index.js
@@ -3,5 +3,14 @@ let sumOne=document.querySelector('.sum1');
 let sumTwo=document.querySelector('.sum2');
 let sumThree=document.querySelector('.sum3');
 let sumFour=document.querySelector('.sum4');
+let button=document.querySelector('.discountButton');
 
-console.log(sumOne.innerHTML);
+result.textContent = (`${Number(sumOne.innerHTML)+Number(sumTwo.innerHTML)+Number(sumThree.innerHTML)+Number(sumFour.innerHTML)} руб.`);
+
+button.addEventListener('click', makeDiscount); 
+function makeDiscount() {
+    let countDiscount=(`${Number(sumOne.innerHTML)*20/100+Number(sumTwo.innerHTML)*20/100+Number(sumThree.innerHTML)*20/100+Number(sumFour.innerHTML)*20/100}`);
+    result.textContent = (`${Number(sumOne.innerHTML)+Number(sumTwo.innerHTML)+Number(sumThree.innerHTML)+Number(sumFour.innerHTML) - countDiscount} руб.`); 
+    button.disabled=true;
+}
+
